f5606d2cd30e62da3e1ec120f01aac419961c4f2
A.J. Jerath <A.J.Jerath@hotmail.com>
2022-02-13 18:07:47 -0800

    added file structure

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello!');
-})
+const games = require('./routes/games');
+
+// Routes
+app.use('/api/games', games);
 
 app.listen(port, console.log('App stared on port 5000'));
