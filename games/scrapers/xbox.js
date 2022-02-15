46aedc51a7a874a636b483c315caede6adc54b1b
A.J. Jerath <A.J.Jerath@hotmail.com>
2022-02-14 22:42:34 -0800

    added file scraper

---
+++ games/scrapers/xbox.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+const xbox = async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://www.xbox.com/en-US/live/gold#gameswithgold');
+
+    await page.waitForSelector('#root');
+
+    const results = await page.$$eval('#BodyContent #ContentBlockList_9 .gameDivsWrapper section', gameDivs => {
+        return gameDivs.map(gameDiv => {
+            xbGames = {};
+
+            xbGames.image = gameDiv.querySelector('a .containerIMG img').getAttribute('src');
+            xbGames.imageAlt = gameDiv.querySelector('a .containerIMG img').getAttribute('alt');
+            xbGames.title = gameDiv.querySelector('a div .c-heading').innerText;
+            xbGames.date = gameDiv.querySelector('a div p .availDate').innerText;
+
+            return xbGames;
+        })
+    });
+
+    browser.close();
+    
+    return results;
+};
+
+xbox().then((res) => console.log(res))
--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const x = require('../games/scrapers/xbox');
 
 router.get('/xbox', (req, res) => {
     res.send('hello');
